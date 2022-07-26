5f25540542a46a1a7836a6cda2e267929a9f7a54
Artem0714 <arfed96@gmail.com>
2022-07-27 00:31:03 +0300

    FinishTask13

--- TaskLeetCode13.js
+++ TaskLeetCode13.js
@@ -72,34 +72,42 @@ for (let j = 0; j < mas.length; j += 1) {
     }
 }*/
 
-
-
-
-
-const symbol = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-const value = [1, 5, 10, 50, 100, 500, 1000]
-
-let number = function(x) {
-    for (let i = 0; i < symbol.length; i += 1) {
-        if (x == symbol[i]) {
-            return value[i]
-        }
-    }
-}
-
-let mean = function(x) {
+/*let mean = function(x) {
     let NumberArr1 = Array.from(String(x), String);
-    for (let j = 0; j < NumberArr1.length; j += 1) {
+    for (let k = 0; k < NumberArr1.length; k += 1) {
         let NumberArr2 = number(NumberArr1[j])
         if (NumberArr2[j+1] > NumberArr2[j]){
             alert (NumberArr2[j+1] - NumberArr2[j])
         } else {
             alert (NumberArr2[j])
         }
-        
     }
+}*/
+
+
+
+roman = 'XVII'
+
+let romanToInt = function(s) {
+    const symbol = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+    const value = [1, 5, 10, 50, 100, 500, 1000];
+    let NumOfArr = Array.from(String(s), String)
+    let GoGo = new Array ();
+    for (let i = 0; i < NumOfArr.length; i += 1) {
+        for (let j = 0; j < symbol.length; j += 1) {
+            if (NumOfArr[i] == symbol[j]) {
+                GoGo.push (value[j])
+            }    
+        }
+    }
+    for (let k = 0; k < GoGo.length; k += 1) {
+        if (GoGo[k] < GoGo[k+1]) {
+            GoGo[k+1] = GoGo[k+1] - GoGo[k]
+            GoGo.splice(k,1)
+        }
+    }
+    return (GoGo.map(i=>x+=i, x=0).reverse()[0])//нашел это решение загуглив, красиво 
+    // выглядит но пока не шибко понимаю как это работает(я так понимаю это замена обычному циклу)
 }
 
-x = 'LVIII'
-
-alert (mean(x))
+alert (romanToInt(roman))
